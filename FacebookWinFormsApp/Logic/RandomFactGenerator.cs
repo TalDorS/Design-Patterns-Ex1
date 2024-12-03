bb557123ba31dcc7581da91de8102c123ae8d6d0
Adva Rose Levine <121626484+AdvaLevine@users.noreply.github.com>
2024-12-03 19:07:35 +0200

    removed my feature to a different class

---
+++ FacebookWinFormsApp/Logic/RandomFactGenerator.cs
@@ -0,0 +1,94 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures.Logic
+{
+    public class RandomFactGenerator
+    {
+        public enum FactType
+        {
+            Friends,
+            Posts,
+            Age,
+            RelationshipStatus,
+            Hometown,
+            Gender,
+            Birthday
+        }
+
+        private readonly User r_LoggedInUser;
+        private readonly List<FactType> m_Facts;
+        private int m_FactIndex;
+
+        public RandomFactGenerator(User i_LoggedInUser)
+        {
+            r_LoggedInUser = i_LoggedInUser ?? throw new ArgumentNullException(nameof(i_LoggedInUser));
+            m_Facts = new List<FactType>
+            {
+                FactType.Friends,
+                FactType.Posts,
+                FactType.Age,
+                FactType.RelationshipStatus,
+                FactType.Hometown,
+                FactType.Gender,
+                FactType.Birthday
+            };
+            m_FactIndex = 0;
+        }
+
+        public string GenerateNextFact()
+        {
+            if (r_LoggedInUser == null)
+            {
+                throw new InvalidOperationException("User is not logged in.");
+            }
+
+            if (m_FactIndex >= m_Facts.Count)
+            {
+                m_FactIndex = 0;
+            }
+
+            FactType currentFact = m_Facts[m_FactIndex];
+            string fact = string.Empty;
+
+            switch (currentFact)
+            {
+                case FactType.Friends:
+                    fact = $"You have {r_LoggedInUser.Friends.Count} friends!";
+                    break;
+                case FactType.Posts:
+                    fact = $"You've made {r_LoggedInUser.Posts.Count} posts so far!";
+                    break;
+                case FactType.Age:
+                    if (DateTime.TryParse(r_LoggedInUser.Birthday, out DateTime birthday))
+                    {
+                        int age = DateTime.Now.Year - birthday.Year;
+                        if (birthday.Date > DateTime.Now.AddYears(-age)) age--;
+                        fact = $"You are {age} years old!";
+                    }
+                    break;
+                case FactType.RelationshipStatus:
+                    fact = $"Your relationship status is: {r_LoggedInUser.RelationshipStatus}.";
+                    break;
+                case FactType.Hometown:
+                    fact = $"You live in {r_LoggedInUser.Hometown?.Name ?? "an unknown place"}!";
+                    break;
+                case FactType.Gender:
+                    fact = $"Your gender is: {r_LoggedInUser.Gender}.";
+                    break;
+                case FactType.Birthday:
+                    fact = $"You were born on {r_LoggedInUser.Birthday}!";
+                    break;
+            }
+
+            m_FactIndex++;
+
+            return fact;
+        }
+    }
+}
+
--- FacebookWinFormsApp/UserInterface/FormMain.cs
+++ FacebookWinFormsApp/UserInterface/FormMain.cs
@@ -10,26 +10,14 @@ using FacebookWrapper.ObjectModel;
 using FacebookWrapper;
 using System.Windows.Forms.VisualStyles;
 using System.Net.NetworkInformation;
+using BasicFacebookFeatures.Logic;
 
 namespace BasicFacebookFeatures
 {
     public partial class FormMain : Form
     {
-        public enum FactType //todo adva: fix this maybe another class?
-        {
-            Friends,
-            Posts,
-            Age,
-            RelationshipStatus,
-            Hometown,
-            Gender,
-            Birthday
-        }
+        private readonly RandomFactGenerator r_FactGenerator;
 
-        private List<FactType> m_Facts;//todo adva
-        private int m_FactIndex = 0; //todo adva
-
-        private FacebookWrapper.LoginResult m_LoginResult;
         private readonly User r_LoggedInUser = null;
         public bool LogoutButtonClicked { get; private set; }
 
@@ -39,21 +27,10 @@ namespace BasicFacebookFeatures
             LogoutButtonClicked = false;
             FacebookWrapper.FacebookService.s_CollectionLimit = 25;
             r_LoggedInUser = i_LoggedInUser;
-
+            r_FactGenerator = new RandomFactGenerator(i_LoggedInUser);
             pictureBoxProfile.LoadAsync(r_LoggedInUser.PictureNormalURL);
             this.labelUsername.Text = $"Logged in as {r_LoggedInUser.Name}";
 
-            //todo adva: move to a class
-            m_Facts = new List<FactType>
-            {
-                FactType.Friends,
-                FactType.Posts,
-                FactType.Age,
-                FactType.RelationshipStatus,
-                FactType.Hometown,
-                FactType.Gender,
-                FactType.Birthday
-            };
         }
 
         private void buttonLogout_Click(object sender, EventArgs e)
@@ -65,65 +42,20 @@ namespace BasicFacebookFeatures
 
         private void buttonGenerateFact_Click(object sender, EventArgs e)
         {
-            executeGenerte();
+            executeGenertor();
         }
 
-        private void executeGenerte()
+       private void executeGenertor()
         {
             if (r_LoggedInUser != null)
             {
-                string randomFact = generateNextFact();
+                string randomFact = r_FactGenerator.GenerateNextFact();
                 MessageBox.Show(randomFact, "Your Random Fact!");
             }
             else
             {
                 MessageBox.Show("Please log in first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }
-        private string generateNextFact() //todo: adva - move to a class
-        {
-            if (m_FactIndex >= m_Facts.Count)
-            {
-                m_FactIndex = 0; 
-            }
-
-            FactType currentFact = m_Facts[m_FactIndex];
-            string fact = string.Empty;
-
-            
-            switch (currentFact)
-            {
-                case FactType.Friends:
-                    fact = string.Format("You have {0} friends!", r_LoggedInUser.Friends.Count);
-                    break;
-                case FactType.Posts:
-                    fact = string.Format("You've made {0} posts so far!", r_LoggedInUser.Posts.Count);
-                    break;
-                case FactType.Age:
-                    if (DateTime.TryParse(r_LoggedInUser.Birthday, out DateTime birthday))
-                    {
-                        int age = DateTime.Now.Year - birthday.Year;
-                        if (birthday.Date > DateTime.Now.AddYears(-age)) age--;
-                        fact = string.Format("You are {0} years old!", age);
-                    }
-                    break;
-                case FactType.RelationshipStatus:
-                    fact = string.Format("Your relationship status is: {0}.", r_LoggedInUser.RelationshipStatus);
-                    break;
-                case FactType.Hometown:
-                    fact = string.Format("You live in {0}!", r_LoggedInUser.Hometown?.Name);
-                    break;
-                case FactType.Gender:
-                    fact = string.Format("Your gender is: {0}.", r_LoggedInUser.Gender);
-                    break;
-                case FactType.Birthday:
-                    fact = string.Format("You were born on {0}!", r_LoggedInUser.Birthday);
-                    break;
-            }
-
-            m_FactIndex++; 
-
-            return fact;
         }
     }
 }

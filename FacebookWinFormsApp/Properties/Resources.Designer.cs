27dc7e22daca0d985b19b237451d404b0ba40937
Adva Rose Levine <121626484+AdvaLevine@users.noreply.github.com>
2024-12-04 16:05:43 +0200

    .

--- FacebookWinFormsApp/Properties/Resources.Designer.cs
+++ FacebookWinFormsApp/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace BasicFacebookFeatures.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Facebook_logo {
+            get {
+                object obj = ResourceManager.GetObject("Facebook_logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap images {
             get {
                 object obj = ResourceManager.GetObject("images", resourceCulture);
